da93ad6dd18dd50fe92b804c462f6995c4263d4c
LERO08 <leninsolisterrazas2008@gmail.com>
2026-02-01 23:05:40 -0300

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Camy Sport</h1>
+      <p>La web ya funciona 🎉</p>
+    </div>
+  );
+}
