e6674ca1ae1f35c59477527d529b73a12b842307
LERO08 <leninsolisterrazas2008@gmail.com>
2026-02-01 23:02:49 -0300

    Delete src directory

--- src/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Camy Sport</h1>
-      <p>La web ya funciona 🎉</p>
-    </div>
-  );
-}
